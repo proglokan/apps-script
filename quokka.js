3d6b4a7feb598964a7aa1149c5d7906991434504
proglokan <92769441+proglokan@users.noreply.github.com>
2023-03-30 02:55:07 -0700

    Refactor some, new barcode, split ship, next query

--- quokka.js
+++ quokka.js
@@ -1,28 +1,5 @@
 // /*?.*/ → test code speed
-
-const arr = [];
-for (let n = 0; n < 1000; ++n) {
-	arr.push(Math.floor(Math.random() * 10000));
-}
-
-for (let n = 0; n < 100; ++n) {
-	const evenNums = arr.filter((num) => num % 2 === 0); //?.
-}
-
-for (let n = 0; n < 100; ++n) {
-	const evenNums = [];
-	for (let x = 0; x < arr.length; ++x) {
-		if (!(arr[x] % 2)) evenNums.push(arr[x]); //?.
-	}
-}
-
-for (let n = 0; n < 100; ++n) {
-	let sum = arr.reduce((acc, num) => acc + num, 0); //?.
-}
-
-for (let n = 0; n < 100; ++n) {
-	let sum = 0;
-	for (let x = 0; x < arr.length; ++x) {
-		sum += arr[x]; //?.
-	}
-}
+const arrOfStrings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+const string = 'j';
+const regex = new RegExp(string, 'i');
+arrOfStrings.some((num) => regex.test(num)); //?
