49055e3d0ca7e6d2d82422dbb1eece3c84c92c1f
proglokan <92769441+proglokan@users.noreply.github.com>
2023-04-19 03:08:26 -0700

    Organize files and write pgt program

--- benchmark.js
+++
@@ -1,6 +0,0 @@
-const sheet = SpeadsheetApp.getActiveSpreadsheet().getSheetByName('Mar 2023');
-const range = 'AB3:AA';
-const rawVals = sheet.getRange(range).getValues();
-const vals = vals.map((val, index) => {
-	val[1];
-});
---
+++ pgt/pgt-issued.js
@@ -0,0 +1,25 @@
+function pgt() {
+	const sheet = SpeadsheetApp.getActiveSpreadsheet().getSheetByName('Mar 2023');
+	const range = 'AB3:AA';
+	const rawVals = sheet.getRange(range).getValues();
+
+	const links = rawVals
+		.map((val, index) => {
+			if (val[0] !== 'PGT Issued') return null;
+			val = [val[1], index + 3];
+			return val;
+		})
+		.filter(Boolean);
+
+	const template = HtmlService.createTemplateFromFile('pgts.html');
+	template.links = links;
+	const html = template.evaluate().setWidth(955).setHeight(1000);
+	const ui = SpreadsheetApp.getUi();
+	ui.showModelessDialog(html, 'PGTs');
+}
+
+function updatePGTs(printedLinks) {
+	for (const row of printedLinks) {
+		sheet.getRange(row, 28).setValue('PGT Sent');
+	}
+}
